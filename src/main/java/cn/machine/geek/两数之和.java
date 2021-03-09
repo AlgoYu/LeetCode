ff9675ef4082b29783e702ceda2cc78a61ceba1a
MachineGeek <794763733@qq.com>
2021-03-09 16:18:57 +0800

    add algorithm.

--- src/main/java/cn/machine/geek/两数之和.java
+++ src/main/java/cn/machine/geek/两数之和.java
@@ -26,14 +26,15 @@ import java.util.Map;
 public class 两数之和 {
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
-        A : for (int i = 0; i < nums.length; i++){
-            for (int j = nums.length-1; j > i; j--){
-                if(nums[i]+nums[j] == target){
-                    result[0] = i;
-                    result[1] = j;
-                    break A;
-                }
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            int tmp = target - nums[i];
+            if(map.containsKey(tmp)){
+                result[0] = map.get(tmp);
+                result[1] = i;
+                break;
             }
+            map.put(nums[i],i);
         }
         return result;
     }
