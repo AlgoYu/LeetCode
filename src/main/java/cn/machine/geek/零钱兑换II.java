f4b25f9c04ac0f939c199c844953401ec2c70fca
XiaoYu <794763733@qq.com>
2021-06-10 19:49:09 +0800

    add algorithm.

--- src/main/java/cn/machine/geek/零钱兑换II.java
+++ src/main/java/cn/machine/geek/零钱兑换II.java
@@ -37,7 +37,6 @@ package cn.machine.geek;
  */
 public class 零钱兑换II {
     public int change(int amount, int[] coins) {
-        if (amount <= 0 || coins == null || coins.length == 0) return 0;
         int[] dp = new int[amount + 1];
         dp[0] = 1;
         for (int coin : coins) {
