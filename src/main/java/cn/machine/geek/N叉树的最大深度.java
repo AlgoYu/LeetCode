3132651c11c52f6f54336bd1e9117125593ac031
XiaoYu <794763733@qq.com>
2021-07-05 21:29:49 +0800

    add algorithm.

--- src/main/java/cn/machine/geek/N叉树的最大深度.java
+++ src/main/java/cn/machine/geek/N叉树的最大深度.java
@@ -3,10 +3,30 @@ package cn.machine.geek;
 import java.util.List;
 
 /**
- * @Author XiaoYu
- * @Description TODO
- * @Date 2021/6/9 3:34 下午
- * @Email 794763733@qq.com
+ 二叉树的最小深度
+ 给定一个二叉树，找出其最小深度。
+
+ 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+
+ 说明：叶子节点是指没有子节点的节点。
+
+
+
+ 示例 1：
+
+
+ 输入：root = [3,9,20,null,null,15,7]
+ 输出：2
+ 示例 2：
+
+ 输入：root = [2,null,3,null,4,null,5,null,6]
+ 输出：5
+
+
+ 提示：
+
+ 树中节点数的范围在 [0, 105] 内
+ -1000 <= Node.val <= 1000
  */
 public class N叉树的最大深度 {
     // Definition for a Node.
--- src/main/java/cn/machine/geek/二叉树的后序遍历.java
+++ src/main/java/cn/machine/geek/二叉树的后序遍历.java
@@ -4,10 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @Author XiaoYu
- * @Description TODO
- * @Date 2021/6/6 6:38 下午
- * @Email 794763733@qq.com
+ * 二叉树的后序遍历
+ * 给定一个二叉树，返回它的 后序 遍历。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [1,null,2,3]
+ * 1
+ * \
+ * 2
+ * /
+ * 3
+ * <p>
+ * 输出: [3,2,1]
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
  */
 public class 二叉树的后序遍历 {
     public class TreeNode {
