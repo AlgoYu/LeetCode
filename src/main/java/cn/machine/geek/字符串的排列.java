281cb3a5599ef376a145c2ead20b68b6c84614a1
XiaoYu <794763733@qq.com>
2021-07-27 23:44:15 +0800

    add algorithm

---
+++ src/main/java/cn/machine/geek/字符串的排列.java
@@ -0,0 +1,51 @@
+package cn.machine.geek;
+
+/**
+ * 字符串的排列
+ * 给定两个字符串 s1 和 s2，写一个函数来判断 s2 是否包含 s1 的排列。
+ * <p>
+ * 换句话说，第一个字符串的排列之一是第二个字符串的 子串 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入: s1 = "ab" s2 = "eidbaooo"
+ * 输出: True
+ * 解释: s2 包含 s1 的排列之一 ("ba").
+ * 示例 2：
+ * <p>
+ * 输入: s1= "ab" s2 = "eidboaoo"
+ * 输出: False
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= s1.length, s2.length <= 104
+ * s1 和 s2 仅包含小写字母
+ */
+public class 字符串的排列 {
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s1.length(), m = s2.length();
+        if (n > m) {
+            return false;
+        }
+        int[] cnt = new int[26];
+        for (int i = 0; i < n; ++i) {
+            --cnt[s1.charAt(i) - 'a'];
+        }
+        int left = 0;
+        for (int right = 0; right < m; ++right) {
+            int x = s2.charAt(right) - 'a';
+            ++cnt[x];
+            while (cnt[x] > 0) {
+                --cnt[s2.charAt(left) - 'a'];
+                ++left;
+            }
+            if (right - left + 1 == n) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
