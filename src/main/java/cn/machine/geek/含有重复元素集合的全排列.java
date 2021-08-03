0ee40620f3b67ec7e70c8968ad7f3482985223e7
XiaoYu <794763733@qq.com>
2021-08-03 19:35:18 +0800

    add algorithm.

---
+++ src/main/java/cn/machine/geek/含有重复元素集合的全排列.java
@@ -0,0 +1,59 @@
+package cn.machine.geek;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 剑指 Offer II 084. 含有重复元素集合的全排列
+ * 给定一个可包含重复数字的整数集合 nums ，按任意顺序 返回它所有不重复的全排列。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,1,2]
+ * 输出：
+ * [[1,1,2],
+ * [1,2,1],
+ * [2,1,1]]
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 8
+ * -10 <= nums[i] <= 10
+ */
+public class 含有重复元素集合的全排列 {
+    boolean[] vis;
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        List<Integer> perm = new ArrayList<Integer>();
+        vis = new boolean[nums.length];
+        Arrays.sort(nums);
+        backtrack(nums, ans, 0, perm);
+        return ans;
+    }
+
+    public void backtrack(int[] nums, List<List<Integer>> ans, int idx, List<Integer> perm) {
+        if (idx == nums.length) {
+            ans.add(new ArrayList<Integer>(perm));
+            return;
+        }
+        for (int i = 0; i < nums.length; ++i) {
+            if (vis[i] || (i > 0 && nums[i] == nums[i - 1] && !vis[i - 1])) {
+                continue;
+            }
+            perm.add(nums[i]);
+            vis[i] = true;
+            backtrack(nums, ans, idx + 1, perm);
+            vis[i] = false;
+            perm.remove(idx);
+        }
+    }
+}
