3511aaa7a13fac22a0d7ab00550f7f2bc14967a7
XiaoYu <794763733@qq.com>
2021-10-12 16:51:57 +0800

    add algorithm.

---
+++ src/main/java/cn/machine/geek/换酒问题.java
@@ -0,0 +1,20 @@
+package cn.machine.geek;
+
+/**
+ * @Author XiaoYu
+ * @Description TODO
+ * @Date 2021/10/12 4:27 下午
+ * @Email 794763733@qq.com
+ */
+public class 换酒问题 {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int empty = numBottles;
+        int count = numBottles;
+        while (empty >= numExchange) {
+            int newBottles = empty / numExchange;
+            count += newBottles;
+            empty = empty - (newBottles * numExchange) + newBottles;
+        }
+        return count;
+    }
+}
