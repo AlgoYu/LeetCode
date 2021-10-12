c067ae4f35639b074a2cbc1b78308bfb1f882727
XiaoYu <794763733@qq.com>
2021-10-12 16:17:53 +0800

    add algorithm.

--- src/main/java/cn/machine/geek/两数相除.java
+++ src/main/java/cn/machine/geek/两数相除.java
@@ -8,28 +8,30 @@ package cn.machine.geek;
  */
 public class 两数相除 {
     public int divide(int dividend, int divisor) { // 被除数 除数
-        if (divisor == -1 && dividend == Integer.MIN_VALUE) return Integer.MAX_VALUE; // 溢出
-        int sign = 1;
-        if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0))
-            sign = -1;
-        int a = dividend > 0 ? -dividend : dividend;
-        int b = divisor > 0 ? -divisor : divisor;
-        // 都改为负号是因为int 的范围是[2^31, 2^31-1]，如果a是-2^32，转为正数时将会溢出
-        //System.out.println(a + " " + b);
-        if (a > b) return 0;
-        int ans = div(a, b);
-        return sign == -1 ? -ans : ans;
-    }
-
-    private static int div(int a, int b) {
-        if (a > b) return 0;
-        int count = 1;
-        int tb = b;
-        while (tb + tb >= a && tb + tb < 0) { // 溢出之后不再小于0
-            tb += tb;
-            count += count;
-            //System.out.println(tb + " " + count + " " + count*b);
+        long dividendL = dividend;
+        long divisorL = divisor;
+        // 一个数正、一个数负数
+        boolean flag = false;
+        if (dividendL > 0 ^ divisorL > 0) {
+            flag = true;
         }
-        return count + div(a - tb, b);
+        long result = 0;
+        dividendL = Math.abs(dividendL);
+        divisorL = Math.abs(divisorL);
+        if (divisorL != 1) {
+            while (dividendL >= divisorL) {
+                dividendL -= divisorL;
+                result++;
+            }
+        } else {
+            result = dividendL;
+        }
+        if (flag) {
+            result = -result;
+        }
+        if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+            result = Integer.MAX_VALUE;
+        }
+        return (int) result;
     }
 }
