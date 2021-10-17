6f779c23c42f44f33ef69bb81a011fbdc4aa412b
XiaoYu <794763733@qq.com>
2021-10-17 12:05:52 +0800

    add algorithm.

---
+++ src/main/java/cn/machine/geek/第k个数.java
@@ -0,0 +1,13 @@
+package cn.machine.geek;
+
+/**
+ * @Author XiaoYu
+ * @Description TODO
+ * @Date 2021/10/6 5:16 下午
+ * @Email 794763733@qq.com
+ */
+public class 第k个数 {
+    public int getKthMagicNumber(int k) {
+        return 0;
+    }
+}
