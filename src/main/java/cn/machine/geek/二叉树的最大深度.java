2ea7e2ee741d70d5793cb4af966372d5ffec46de
XiaoYu <794763733@qq.com>
2021-10-23 18:12:29 +0800

    add algorithm.

---
+++ src/main/java/cn/machine/geek/二叉树的最大深度.java
@@ -0,0 +1,35 @@
+package cn.machine.geek;
+
+/**
+ * @Author XiaoYu
+ * @Description TODO
+ * @Date 2021/10/23 6:10 下午
+ * @Email 794763733@qq.com
+ */
+public class 二叉树的最大深度 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
