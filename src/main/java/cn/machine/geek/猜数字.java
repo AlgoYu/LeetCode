fbb0f25e8a06ac0d8ab32830a5027fe4dbd19ff7
XiaoYu <794763733@qq.com>
2021-10-25 18:42:05 +0800

    add algorithm.

---
+++ src/main/java/cn/machine/geek/猜数字.java
@@ -0,0 +1,19 @@
+package cn.machine.geek;
+
+/**
+ * @Author XiaoYu
+ * @Description TODO
+ * @Date 2021/10/25 6:41 下午
+ * @Email 794763733@qq.com
+ */
+public class 猜数字 {
+    public int game(int[] guess, int[] answer) {
+        int count = 0;
+        for (int i = 0; i < guess.length; i++) {
+            if (guess[i] == answer[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
