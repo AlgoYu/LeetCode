31f13f4c10c67604df02cd3ce115e3abf21a6656
xiaoyu <794763733@qq.co>
2021-11-06 01:08:44 +0800

    1

---
+++ src/main/java/cn/machine/geek/丢失的数字.java
@@ -0,0 +1,15 @@
+package cn.machine.geek;
+
+public class 丢失的数字 {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int m = 0;
+        for (int i = 0; i <= n; i++) {
+            m ^= i;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            m ^= nums[i];
+        }
+        return m;
+    }
+}
