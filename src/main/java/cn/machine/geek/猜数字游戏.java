2e5dbbd09d5ef15d96d93db18b8c7dad05b293f1
xiaoyu <794763733@qq.co>
2021-11-08 20:49:20 +0800

    1

---
+++ src/main/java/cn/machine/geek/猜数字游戏.java
@@ -0,0 +1,32 @@
+package cn.machine.geek;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 猜数字游戏 {
+    public String getHint(String secret, String guess) {
+        int n = secret.length();
+        Map<Character, Integer> map = new HashMap<>();
+        int m = 0;
+        for (int i = 0; i < n; i++) {
+            char ch = secret.charAt(i);
+            if (ch == guess.charAt(i)) {
+                m++;
+            } else {
+                map.put(ch, map.getOrDefault(ch, 0) + 1);
+            }
+        }
+        int k = 0;
+        for (int i = 0; i < n; i++) {
+            char ch = guess.charAt(i);
+            if (secret.charAt(i) != ch) {
+                Integer num = map.getOrDefault(ch, 0);
+                if (num > 0) {
+                    k++;
+                    map.put(ch, num - 1);
+                }
+            }
+        }
+        return m + "A" + k + "B";
+    }
+}
