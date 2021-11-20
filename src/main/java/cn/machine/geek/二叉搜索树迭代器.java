bec3eaa330fb69c9e225edbf73f8f3210ff23115
xiaoyu <794763733@qq.co>
2021-11-20 21:04:25 +0800

    1

---
+++ src/main/java/cn/machine/geek/二叉搜索树迭代器.java
@@ -0,0 +1,55 @@
+package cn.machine.geek;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class 二叉搜索树迭代器 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    List<TreeNode> list;
+    int index;
+
+    public 二叉搜索树迭代器(TreeNode root) {
+        list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                cur = stack.pop();
+                list.add(cur);
+                cur = cur.right;
+            }
+        }
+        index = 0;
+    }
+
+    public int next() {
+        return list.get(index++).val;
+    }
+
+    public boolean hasNext() {
+        return index < list.size();
+    }
+}
