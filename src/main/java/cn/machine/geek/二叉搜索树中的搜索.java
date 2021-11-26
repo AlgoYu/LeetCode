56bc310df0ea7eab72af79c073f5468d8fd9a08c
xiaoyu <794763733@qq.co>
2021-11-26 14:05:33 +0800

    1

---
+++ src/main/java/cn/machine/geek/二叉搜索树中的搜索.java
@@ -0,0 +1,33 @@
+package cn.machine.geek;
+
+public class 二叉搜索树中的搜索 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        }
+        TreeNode leftRoot = searchBST(root.left, val);
+        return leftRoot == null ? searchBST(root.right, val) : leftRoot;
+    }
+}
