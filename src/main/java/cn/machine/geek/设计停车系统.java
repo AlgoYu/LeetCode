133e9dd89b40d6b213cace93e8939815486bd45c
xiaoyu <794763733@qq.co>
2021-12-05 22:36:21 +0800

    1

---
+++ src/main/java/cn/machine/geek/设计停车系统.java
@@ -0,0 +1,20 @@
+package cn.machine.geek;
+
+public class 设计停车系统 {
+    int[] capacity;
+
+    public 设计停车系统(int big, int medium, int small) {
+        capacity = new int[3];
+        capacity[0] = big;
+        capacity[1] = medium;
+        capacity[2] = small;
+    }
+
+    public boolean addCar(int carType) {
+        if (capacity[carType - 1] == 0) {
+            return false;
+        }
+        capacity[carType - 1]--;
+        return true;
+    }
+}
