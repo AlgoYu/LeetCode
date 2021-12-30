513d81e640f9e379d91bce11bb0d519178ac8bea
xiaoyu <794763733@qq.co>
2021-12-31 00:16:12 +0800

    1

---
+++ src/main/java/cn/machine/geek/完美数.java
@@ -0,0 +1,16 @@
+package cn.machine.geek;
+
+public class 完美数 {
+    public boolean checkPerfectNumber(int num) {
+        if (num == 1) {
+            return false;
+        }
+        int count = 1;
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                count += i;
+            }
+        }
+        return count == num;
+    }
+}
