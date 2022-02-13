2c6a7b771a4cfd28f64a16260dcd847fee809463
xiaoyu <794763733@qq.com>
2022-02-13 16:00:04 +0800

    1

---
+++ src/main/java/cn/machine/geek/气球的最大数量.java
@@ -0,0 +1,43 @@
+package cn.machine.geek;
+
+public class 气球的最大数量 {
+    public int maxNumberOfBalloons(String text) {
+        int[] count = new int[5];
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            switch (ch) {
+                case 'b':
+                    count[0]++;
+                    break;
+                case 'a':
+                    count[1]++;
+                    break;
+                case 'l':
+                    count[2]++;
+                    break;
+                case 'o':
+                    count[3]++;
+                    break;
+                case 'n':
+                    count[4]++;
+                    break;
+            }
+        }
+        int total = 0;
+        X:
+        for (; ; ) {
+            count[0]--;
+            count[1]--;
+            count[2] -= 2;
+            count[3] -= 2;
+            count[4]--;
+            for (int i = 0; i < count.length; i++) {
+                if (count[i] < 0) {
+                    break X;
+                }
+            }
+            total++;
+        }
+        return total;
+    }
+}
