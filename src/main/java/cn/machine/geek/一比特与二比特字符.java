fa29ad786f088092b9181a987d16cdb856ca8ca5
xiaoyu <794763733@qq.com>
2022-02-20 20:15:51 +0800

    1

---
+++ src/main/java/cn/machine/geek/一比特与二比特字符.java
@@ -0,0 +1,11 @@
+package cn.machine.geek;
+
+public class 一比特与二比特字符 {
+    public boolean isOneBitCharacter(int[] bits) {
+        int n = bits.length, i = 0;
+        while (i < n - 1) {
+            i += bits[i] + 1;
+        }
+        return i == n - 1;
+    }
+}
