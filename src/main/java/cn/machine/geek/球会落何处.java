370a9ccd7730c3b1c0e9331bbe0565ddae158ce5
xiaoyu <794763733@qq.com>
2022-02-24 20:25:57 +0800

    1

---
+++ src/main/java/cn/machine/geek/球会落何处.java
@@ -0,0 +1,31 @@
+package cn.machine.geek;
+
+public class 球会落何处 {
+    public int[] findBall(int[][] grid) {
+        int[] answer = new int[grid[0].length];
+        for (int i = 0; i < grid[0].length; i++) {
+            answer[i] = dfs(grid, 0, i);
+        }
+        return answer;
+    }
+
+    private int dfs(int[][] grid, int level, int col) {
+        if (level >= grid.length) {
+            return col;
+        }
+        int board = grid[level][col];
+        if (board == 1 && col + 1 < grid[level].length && grid[level][col + 1] == 1) {
+            return dfs(grid, level + 1, col + 1);
+        }
+        if (board == -1 && col - 1 >= 0 && grid[level][col - 1] == -1) {
+            return dfs(grid, level + 1, col - 1);
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        球会落何处 test = new 球会落何处();
+        test.findBall(new int[][]{{1, 1, 1, 1, 1, 1}, {-1, -1, -1, -1, -1, -1}, {1, 1, 1, 1, 1, 1}, {-1, -1, -1, -1, -1, -1}});
+        test.findBall(new int[][]{{1, 1, 1, -1, -1}, {1, 1, 1, -1, -1}, {-1, -1, -1, 1, 1}, {1, 1, 1, 1, -1}, {-1, -1, -1, -1, -1}});
+    }
+}
