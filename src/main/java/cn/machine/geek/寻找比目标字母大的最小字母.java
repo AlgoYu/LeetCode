822032f535c0e78de879ff2b355e10d92c53b962
xiaoyu <794763733@qq.com>
2022-04-03 21:50:01 +0800

    1

--- src/main/java/cn/machine/geek/寻找比目标字母大的最小字母.java
+++ src/main/java/cn/machine/geek/寻找比目标字母大的最小字母.java
@@ -7,22 +7,19 @@ package cn.machine.geek;
  * @Email 794763733@qq.com
  */
 public class 寻找比目标字母大的最小字母 {
+
     public char nextGreatestLetter(char[] letters, char target) {
-        int left = 0;
-        int right = letters.length;
+        int n = letters.length;
+        int left = 0, right = n;
         while (left < right) {
             int mid = left + ((right - left) >> 1);
-            if (target >= letters[mid]) {
+            if (letters[mid] <= target) {
                 left = mid + 1;
             } else {
                 right = mid;
             }
         }
-        return left >= letters.length || left <= 0 ? letters[0] : letters[left];
+        return left == n ? letters[0] : letters[left];
     }
 
-    public static void main(String[] args) {
-        寻找比目标字母大的最小字母 test = new 寻找比目标字母大的最小字母();
-        test.nextGreatestLetter(new char[]{'c', 'f', 'j'}, 'd');
-    }
 }
