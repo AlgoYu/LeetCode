7f568ae8d4db12453d3d8c97fb2451570efc7d0f
xiaoyu <794763733@qq.com>
2022-04-17 23:18:10 +0800

    1

---
+++ src/main/java/cn/machine/geek/最常见的单词.java
@@ -0,0 +1,49 @@
+package cn.machine.geek;
+
+import java.util.*;
+
+public class 最常见的单词 {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        if (paragraph.isEmpty()) {
+            return paragraph;
+        }
+        Map<String, Integer> count = new HashMap<>();
+        Set<String> set = new HashSet<>(Arrays.asList(banned));
+
+        char[] chars = paragraph.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        String maxWord = "";
+        int maxNum = 0;
+        for (int i = 0; i < chars.length; i++) {
+            char ch = chars[i];
+            if (Character.isLetter(ch)) {
+                sb.append(ch);
+                continue;
+            }
+            if (sb.length() > 0) {
+                String word = sb.toString().toLowerCase();
+                sb.delete(0, sb.length());
+                if (!set.contains(word)) {
+                    int num = count.getOrDefault(word, 0) + 1;
+                    count.put(word, num);
+                    if (num > maxNum) {
+                        maxNum = num;
+                        maxWord = word;
+                    }
+                }
+            }
+        }
+        if (sb.length() > 0) {
+            String word = sb.toString().toLowerCase();
+            sb.delete(0, sb.length());
+            if (!set.contains(word)) {
+                int num = count.getOrDefault(word, 0) + 1;
+                count.put(word, num);
+                if (num > maxNum) {
+                    maxWord = word;
+                }
+            }
+        }
+        return maxWord;
+    }
+}
