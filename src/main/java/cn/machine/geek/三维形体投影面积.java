22579880dd26568a52d95e117dc31b4c76d93eae
xiaoyu <794763733@qq.com>
2022-04-26 13:29:54 +0800

    1

---
+++ src/main/java/cn/machine/geek/三维形体投影面积.java
@@ -0,0 +1,27 @@
+package cn.machine.geek;
+
+public class 三维形体投影面积 {
+    public int projectionArea(int[][] grid) {
+        int n = grid.length;
+        if (n == 0) {
+            return 0;
+        }
+        int top = 0;
+        int side = 0;
+        int front = 0;
+        for (int i = 0; i < n; i++) {
+            int maxRow = 0;
+            int maxCol = 0;
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] > 0) {
+                    top++;
+                }
+                maxRow = Math.max(maxRow, grid[i][j]);
+                maxCol = Math.max(maxCol, grid[j][i]);
+            }
+            side += maxRow;
+            front += maxCol;
+        }
+        return top + side + front;
+    }
+}
