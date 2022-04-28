d06d5828412812635952d2bfa82a02ed402892e4
XiaoYu <794763733@qq.com>
2022-04-28 11:13:17 +0800

    1

---
+++ src/main/java/cn/machine/geek/按奇偶排序数组.java
@@ -0,0 +1,19 @@
+package cn.machine.geek;
+
+public class 按奇偶排序数组 {
+    public int[] sortArrayByParity(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            while (left < right && (nums[left] & 1) == 0) {
+                left++;
+            }
+            while (left < right && (nums[right] & 1) == 1) {
+                right--;
+            }
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+        }
+        return nums;
+    }
+}
