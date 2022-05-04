24189c1c9d536ebe160d320d1690d00f371fe191
XiaoYu <794763733@qq.com>
2022-05-04 17:31:52 +0800

    1

---
+++ src/main/java/cn/machine/geek/乘积小于K的子数组.java
@@ -0,0 +1,19 @@
+package cn.machine.geek;
+
+public class 乘积小于K的子数组 {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int n = 0;
+        int left = 0, right = 0;
+        int answer = 1;
+        while (right < nums.length) {
+            answer *= nums[right];
+            while (answer >= k && left <= right) {
+                answer /= nums[left];
+                left++;
+            }
+            n += right - left + 1;
+            right++;
+        }
+        return n;
+    }
+}
