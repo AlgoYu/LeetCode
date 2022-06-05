9586b718f4b3cf24cc1d5cec11d0564653972fcd
XiaoYu <794763733@qq.com>
2022-06-05 14:41:48 +0800

    1

---
+++ src/main/java/cn/machine/geek/在圆内随机生成点.java
@@ -0,0 +1,25 @@
+package cn.machine.geek;
+
+import java.util.Random;
+
+public class 在圆内随机生成点 {
+    Random random;
+    double xc, yc, r;
+
+    public 在圆内随机生成点(double radius, double x_center, double y_center) {
+        random = new Random();
+        xc = x_center;
+        yc = y_center;
+        r = radius;
+    }
+
+    public double[] randPoint() {
+        while (true) {
+            double x = random.nextDouble() * (2 * r) - r;
+            double y = random.nextDouble() * (2 * r) - r;
+            if (x * x + y * y <= r * r) {
+                return new double[]{xc + x, yc + y};
+            }
+        }
+    }
+}
