8808cabd4e7e58fb00c2d62d0c7806836c5e6d67
AlgoYu <794763733@qq.com>
2022-09-20 14:01:24 +0800

    1

---
+++ src/main/java/cn/machine/geek/划分为k个相等的子集.java
@@ -0,0 +1,39 @@
+package cn.machine.geek;
+
+import java.util.Arrays;
+
+public class 划分为k个相等的子集 {
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        int all = Arrays.stream(nums).sum();
+        if (all % k != 0) {
+            return false;
+        }
+        int per = all / k;
+        Arrays.sort(nums);
+        int n = nums.length;
+        if (nums[n - 1] > per) {
+            return false;
+        }
+        boolean[] dp = new boolean[1 << n];
+        int[] curSum = new int[1 << n];
+        dp[0] = true;
+        for (int i = 0; i < 1 << n; i++) {
+            if (!dp[i]) {
+                continue;
+            }
+            for (int j = 0; j < n; j++) {
+                if (curSum[i] + nums[j] > per) {
+                    break;
+                }
+                if (((i >> j) & 1) == 0) {
+                    int next = i | (1 << j);
+                    if (!dp[next]) {
+                        curSum[next] = (curSum[i] + nums[j]) % per;
+                        dp[next] = true;
+                    }
+                }
+            }
+        }
+        return dp[(1 << n) - 1];
+    }
+}
