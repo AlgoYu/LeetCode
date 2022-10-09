e3946d7700114ff8d6c51b794ce1b3e6a481d6f5
AlgoYu <794763733@qq.com>
2022-10-09 20:18:36 +0800

    1

---
+++ src/main/java/cn/machine/geek/括号的分数.java
@@ -0,0 +1,24 @@
+package cn.machine.geek;
+
+import java.util.Stack;
+
+public class 括号的分数 {
+    public int scoreOfParentheses(String s) {
+        int n = s.length();
+        if (n == 2) {
+            return 1;
+        }
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(0);
+            } else {
+                int top = stack.pop();
+                top = stack.pop() + Math.max(top * 2, 1);
+                stack.push(top);
+            }
+        }
+        return stack.peek();
+    }
+}
