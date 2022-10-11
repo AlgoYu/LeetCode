2e3d0c804ddb7bb3a28b1c4537ec7d0826859ccf
AlgoYu <794763733@qq.com>
2022-10-11 13:39:54 +0800

    1

---
+++ src/main/java/cn/machine/geek/仅执行一次字符串交换能否使两个字符串相等.java
@@ -0,0 +1,28 @@
+package cn.machine.geek;
+
+public class 仅执行一次字符串交换能否使两个字符串相等 {
+    public boolean areAlmostEqual(String s1, String s2) {
+        int n = s1.length();
+        int one = -1, two = -1;
+        for (int i = 0; i < n; i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                if (one == -1) {
+                    one = i;
+                    continue;
+                }
+                if (two == -1) {
+                    two = i;
+                    continue;
+                }
+                return false;
+            }
+        }
+        if (one == -1) {
+            return true;
+        }
+        if (two == -1) {
+            return false;
+        }
+        return s1.charAt(one) == s2.charAt(two) && s1.charAt(two) == s2.charAt(one);
+    }
+}
