53d5794d0a0360f2a23f09c17eb50993a069495b
AlgoYu <794763733@qq.com>
2022-10-13 19:03:19 +0800

    1

---
+++ src/main/java/cn/machine/geek/按摩师.java
@@ -0,0 +1,25 @@
+package cn.machine.geek;
+
+public class 按摩师 {
+    public int massage(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return nums[0];
+        }
+        if (n == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        int dp0 = 0, dp1 = nums[0];
+        for (int i = 1; i < n; i++) {
+            int tdp0 = Math.max(dp0, dp1);
+            int tdp1 = dp0 + nums[i];
+
+            dp0 = tdp0;
+            dp1 = tdp1;
+        }
+        return Math.max(dp0, dp1);
+    }
+}
