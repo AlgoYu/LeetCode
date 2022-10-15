a28426c4ce476fab6106abc29547ffd5f5ef0619
AlgoYu <794763733@qq.com>
2022-10-16 02:18:31 +0800

    1

---
+++ src/main/java/cn/machine/geek/下载插件.java
@@ -0,0 +1,13 @@
+package cn.machine.geek;
+
+public class 下载插件 {
+    public int leastMinutes(int n) {
+        // 设置一个动态规划数组
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.min(dp[i - 1] + 1, dp[(i + 1) / 2] + 1);
+        }
+        return dp[n];
+    }
+}
