01f5d31e030e593268db9ec04028f5f524e1293c
AlgoYu <794763733@qq.com>
2022-10-19 13:14:02 +0800

    1

---
+++ src/main/java/cn/machine/geek/无法吃午餐的学生数量.java
@@ -0,0 +1,40 @@
+package cn.machine.geek;
+
+public class 无法吃午餐的学生数量 {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int n = students.length;
+        if (n == 1) {
+            return students[0] == sandwiches[0] ? 0 : 1;
+        }
+        boolean[] eat = new boolean[n];
+        int sa = 0;
+        int sb = 0;
+        for (int i = 0; i < n; i++) {
+            if (students[i] == 0) {
+                sa++;
+            } else {
+                sb++;
+            }
+        }
+        int queneIndex = 0;
+        int i = 0;
+        for (; i < n; i++) {
+            if (sandwiches[i] == 0 && sa == 0) {
+                break;
+            }
+            if (sandwiches[i] == 1 && sb == 0) {
+                break;
+            }
+            while (eat[queneIndex] || students[queneIndex] != sandwiches[i]) {
+                queneIndex = (queneIndex + 1) % n;
+            }
+            eat[queneIndex] = true;
+            if (students[queneIndex] == 0) {
+                sa--;
+            } else {
+                sb--;
+            }
+        }
+        return n - i;
+    }
+}
