f3ca6948094c89b72702821c8a50883963692af5
AlgoYu <794763733@qq.com>
2022-10-21 18:09:50 +0800

    1

---
+++ src/main/java/cn/machine/geek/股票价格跨度.java
@@ -0,0 +1,24 @@
+package cn.machine.geek;
+
+import java.util.Stack;
+
+public class 股票价格跨度 {
+    Stack<int[]> stack;
+    int index;
+
+    public 股票价格跨度() {
+        stack = new Stack<>();
+        stack.push(new int[]{Integer.MAX_VALUE, -1});
+        index = -1;
+    }
+
+    public int next(int price) {
+        index++;
+        while (stack.peek()[0] <= price) {
+            stack.pop();
+        }
+        int result = index - stack.peek()[1];
+        stack.push(new int[]{price, index});
+        return result;
+    }
+}
