7a57742a5bc5e5105510e9e94f3e4c2947dde288
AlgoYu <794763733@qq.com>
2022-10-29 23:03:59 +0800

    1

---
+++ src/main/java/cn/machine/geek/统计匹配检索规则的物品数量.java
@@ -0,0 +1,27 @@
+package cn.machine.geek;
+
+import java.util.List;
+
+public class 统计匹配检索规则的物品数量 {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int index = 0;
+        switch (ruleKey) {
+            case "type":
+                index = 0;
+                break;
+            case "color":
+                index = 1;
+                break;
+            case "name":
+                index = 2;
+                break;
+        }
+        int total = 0;
+        for (List<String> item : items) {
+            if (ruleValue.equals(item.get(index))) {
+                total++;
+            }
+        }
+        return total;
+    }
+}
