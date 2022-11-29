52bbe0e96a76eab7177df54003d9fa4c4c6cb83f
AlgoYu <794763733@qq.com>
2022-11-29 18:22:05 +0800

    1

---
+++ src/main/java/cn/machine/geek/生成交替二进制字符串的最少操作数.java
@@ -0,0 +1,24 @@
+package cn.machine.geek;
+
+public class 生成交替二进制字符串的最少操作数 {
+    public int minOperations(String s) {
+        int totalA = 0;
+        int totalB = 0;
+        char[] chars = s.toCharArray();
+        char a = '0';
+        char b = '1';
+        char tmp;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] != a) {
+                totalA++;
+            }
+            if (chars[i] != b) {
+                totalB++;
+            }
+            tmp = a;
+            a = b;
+            b = tmp;
+        }
+        return Math.min(totalA, totalB);
+    }
+}
