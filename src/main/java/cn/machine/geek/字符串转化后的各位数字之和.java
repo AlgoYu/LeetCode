0e70ec5d6ea02c43b68ca0f26566d66e554a52fc
AlgoYu <794763733@qq.com>
2022-12-15 12:12:45 +0800

    1

---
+++ src/main/java/cn/machine/geek/字符串转化后的各位数字之和.java
@@ -0,0 +1,28 @@
+package cn.machine.geek;
+
+public class 字符串转化后的各位数字之和 {
+    public int getLucky(String s, int k) {
+        String temp = convert(s);
+        int result = 0;
+        for (int i = 0; i < k; i++) {
+            result = 0;
+            for (int j = 0; j < temp.length(); j++) {
+                char c = temp.charAt(j);
+                result = result + (c - '0');
+            }
+            temp = String.valueOf(result);
+            if (temp.length() == 1) {
+                return result;
+            }
+        }
+        return result;
+    }
+
+    private String convert(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            sb.append((s.charAt(i) - 'a' + 1));
+        }
+        return sb.toString();
+    }
+}
