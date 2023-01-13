ec1b4ebc0eda7ba1b96bf39b8fdf4255b367a30f
AlgoYu <794763733@qq.com>
2023-01-13 16:20:09 +0800

    1

---
+++ src/main/java/cn/machine/geek/重排字符形成目标字符串.java
@@ -0,0 +1,25 @@
+package cn.machine.geek;
+
+import java.util.Map;
+
+public class 重排字符形成目标字符串 {
+    public int rearrangeCharacters(String s, String target) {
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+        int num = 0;
+        X:
+        while (true) {
+            for (int i = 0; i < target.length(); i++) {
+                int index = target.charAt(i) - 'a';
+                count[index]--;
+                if (count[index] < 0) {
+                    break X;
+                }
+            }
+            num++;
+        }
+        return num;
+    }
+}
