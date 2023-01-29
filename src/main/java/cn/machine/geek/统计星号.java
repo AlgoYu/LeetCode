0fd76dd4183101febd57858b072703cfdef2cee7
AlgoYu <794763733@qq.com>
2023-01-29 17:04:22 +0800

    1

---
+++ src/main/java/cn/machine/geek/统计星号.java
@@ -0,0 +1,18 @@
+package cn.machine.geek;
+
+public class 统计星号 {
+    public int countAsterisks(String s) {
+        int count = 2;
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '|') {
+                count++;
+            }
+            if ((count & 1) == 0 && c == '*') {
+                result++;
+            }
+        }
+        return result;
+    }
+}
