7669914c32da51f8f7a48b6816a5551686a91369
AlgoYu <794763733@qq.com>
2023-02-06 12:52:02 +0800

    1

---
+++ src/main/java/cn/machine/geek/计算布尔二叉树的值.java
@@ -0,0 +1,34 @@
+package cn.machine.geek;
+
+public class 计算布尔二叉树的值 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean evaluateTree(TreeNode root) {
+        if (root.left == null && root.right == null) {
+            return root.val == 1;
+        }
+        if (root.val == 2) {
+            return evaluateTree(root.left) || evaluateTree(root.right);
+        } else {
+            return evaluateTree(root.left) && evaluateTree(root.right);
+        }
+    }
+}
