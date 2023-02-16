1c61689644b84866812f70dfc68309e33510d97f
AlgoYu <794763733@qq.com>
2023-02-16 16:40:15 +0800

    1

---
+++ src/main/java/cn/machine/geek/数组能形成多少数对.java
@@ -0,0 +1,18 @@
+package cn.machine.geek;
+
+public class 数组能形成多少数对 {
+    public int[] numberOfPairs(int[] nums) {
+        int[] count = new int[101];
+        for (int i = 0; i < nums.length; i++) {
+            count[nums[i]]++;
+        }
+        int[] res = new int[2];
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] > 0) {
+                res[0] = res[0] + count[i] / 2;
+            }
+        }
+        res[1] = nums.length - res[0] * 2;
+        return res;
+    }
+}
