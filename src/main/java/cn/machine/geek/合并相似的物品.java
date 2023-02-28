b9b630b4407a33a3b94fa6e34c316fa5d5bd95fc
AlgoYu <794763733@qq.com>
2023-02-28 19:32:59 +0800

    1

---
+++ src/main/java/cn/machine/geek/合并相似的物品.java
@@ -0,0 +1,29 @@
+package cn.machine.geek;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class 合并相似的物品 {
+    public List<List<Integer>> mergeSimilarItems(int[][] items1, int[][] items2) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < items1.length; i++) {
+            List<Integer> ints = map.get(items1[i][0]);
+            if (ints == null) {
+                ints = Arrays.asList(items1[i][0], 0);
+                map.put(items1[i][0], ints);
+            }
+            ints.set(1, ints.get(1) + items1[i][1]);
+        }
+        for (int i = 0; i < items2.length; i++) {
+            List<Integer> ints = map.get(items2[i][0]);
+            if (ints == null) {
+                ints = Arrays.asList(items2[i][0], 0);
+                map.put(items2[i][0], ints);
+            }
+            ints.set(1, ints.get(1) + items2[i][1]);
+        }
+        return map.values().stream().sorted((o1, o2) -> {
+            return o1.get(0) - o2.get(0);
+        }).collect(Collectors.toList());
+    }
+}
