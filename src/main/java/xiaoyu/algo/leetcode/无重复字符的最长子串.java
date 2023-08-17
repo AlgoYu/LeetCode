db51902553899033b63947643c9cb44124c891f6
AlgoYu <794763733@qq.com>
2023-08-17 21:44:05 +0800

    1

--- src/main/java/xiaoyu/algo/leetcode/无重复字符的最长子串.java
+++ src/main/java/xiaoyu/algo/leetcode/无重复字符的最长子串.java
@@ -3,49 +3,12 @@ package xiaoyu.algo.leetcode;
 import java.util.HashSet;
 import java.util.Set;
 
-/**
- * 3. 无重复字符的最长子串
- * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
- * <p>
- * <p>
- * <p>
- * 示例 1:
- * <p>
- * 输入: s = "abcabcbb"
- * 输出: 3
- * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
- * 示例 2:
- * <p>
- * 输入: s = "bbbbb"
- * 输出: 1
- * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
- * 示例 3:
- * <p>
- * 输入: s = "pwwkew"
- * 输出: 3
- * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
- * 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
- * 示例 4:
- * <p>
- * 输入: s = ""
- * 输出: 0
- * <p>
- * <p>
- * 提示：
- * <p>
- * 0 <= s.length <= 5 * 104
- * s 由英文字母、数字、符号和空格组成
- */
 public class 无重复字符的最长子串 {
     public int lengthOfLongestSubstring(String s) {
-        if (s == null || s.length() == 0) {
-            return 0;
-        }
-        int l = 0, r = 0;
-        int n = s.length();
-        int max = 0;
         Set<Character> set = new HashSet<>();
-        while (r < n) {
+        int max = 0;
+        int l = 0, r = 0;
+        while (r < s.length()) {
             char c = s.charAt(r);
             while (l < r && set.contains(c)) {
                 set.remove(s.charAt(l));
