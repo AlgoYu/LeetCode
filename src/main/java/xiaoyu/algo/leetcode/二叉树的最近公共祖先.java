cbb4e11fc84bd20a69d84e36ce5d8b8b1e06e114
AlgoYu <794763733@qq.com>
2023-08-22 06:11:22 +0800

    1

--- src/main/java/xiaoyu/algo/leetcode/二叉树的最近公共祖先.java
+++ src/main/java/xiaoyu/algo/leetcode/二叉树的最近公共祖先.java
@@ -12,14 +12,17 @@ public class 二叉树的最近公共祖先 {
     }
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || root == p || root == q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == p || root == q) {
             return root;
         }
-        TreeNode l = lowestCommonAncestor(root.left, p, q);
-        TreeNode r = lowestCommonAncestor(root.right, p, q);
-        if (l != null && r != null) {
+        TreeNode leftResult = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightResult = lowestCommonAncestor(root.right, p, q);
+        if (leftResult != null && rightResult != null) {
             return root;
         }
-        return l == null ? r : l;
+        return leftResult == null ? rightResult : leftResult;
     }
 }
