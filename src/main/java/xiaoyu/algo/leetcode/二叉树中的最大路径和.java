58ab61c19734bd7c84babe4ad4135793dde9f302
AlgoYu <794763733@qq.com>
2023-09-16 21:39:57 +0800

    1

--- src/main/java/xiaoyu/algo/leetcode/二叉树中的最大路径和.java
+++ src/main/java/xiaoyu/algo/leetcode/二叉树中的最大路径和.java
@@ -31,10 +31,9 @@ public class 二叉树中的最大路径和 {
         if (root == null) {
             return 0;
         }
-        int lm = Math.max(postOrder(root.left), 0);
-        int rm = Math.max(postOrder(root.right), 0);
-
-        max = Math.max(max, root.val + lm + rm);
-        return root.val + Math.max(lm, rm);
+        int lv = Math.max(postOrder(root.left), 0);
+        int rv = Math.max(postOrder(root.right), 0);
+        max = Math.max(max, lv + rv + root.val);
+        return Math.max(lv, rv) + root.val;
     }
 }
